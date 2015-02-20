391e6dff64dd6fa84137a1817b606f4753731bb5
kraigrau <raudevelopment@gmail.com>
2015-02-19 19:57:31 -0500

    this is a test

---
+++ TestApp/scripts/testscript.js
@@ -0,0 +1,15 @@
+﻿function calcSpeedMiles() {
+    var x, text;
+
+    //Get value of the input field with id="weeklyMileage
+    x = document.getElementById("weeklyMileage").value;
+
+    //If x is not a number or less than one
+    if (isNaN(x) || x < 1) {
+        text = "Input not valid";
+    } else {
+        var speedMiles = x * .2;
+        text = speedMiles;
+    }
+    document.getElementById("demo").innerHTML = text;
+}
